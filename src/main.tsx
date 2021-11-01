17c0e1e908d662066191a20ce0c21da983236ae7
AsyaDeWitch <denisevich.asya@gmail.com>
2021-11-01 16:08:26 +0300

    ErrorBoundary. Created class for catching errors in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,41 @@
 import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
 import ReactDom from "react-dom";
+import { History, createBrowserHistory } from "history";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-import { ErrorInfo, PureComponent } from "react";
+import { Component } from "react";
 import Header from "./components/header/header";
 import Footer from "./components/footer";
 import RouteMapper from "./shared/routeMapper";
+import ErrorBoundary from "./shared/errorBoundary";
 
-const Title = "Best Games Market";
+const title = "Best Games Market";
+const history = createBrowserHistory();
 
 interface Props {
   title: string;
+  history: History;
 }
 
-class AppContainer extends PureComponent<Props> {
+class AppContainer extends Component<Props> {
   ["constructor"]: typeof AppContainer;
-
-  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("Uncaught error:", error, errorInfo);
-    alert("Something went wrong...");
-    window.location.href = RouteMapper.Home.url;
-  }
 
   render() {
     return (
-      <Router>
-        <Header title={this.props.title} />
-        <Switch>
-          <Route exact path={RouteMapper.About.url} component={RouteMapper.About.component} />
-          <Route exact path={RouteMapper.Products.url} component={RouteMapper.Products.component} />
-          <Route exact path={RouteMapper.Home.url} component={RouteMapper.Home.component} />
-          <Route>
+      <ErrorBoundary history={this.props.history}>
+        <Router>
+          <Header title={this.props.title} />
+          <Switch>
+            <Route path={RouteMapper.About.url} component={RouteMapper.About.component} />
+            <Route path={RouteMapper.Products.url} component={RouteMapper.Products.component} />
+            <Route exact path={RouteMapper.Home.url} component={RouteMapper.Home.component} />
             <Redirect to={RouteMapper.Home.url} />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
+          </Switch>
+          <Footer />
+        </Router>
+      </ErrorBoundary>
     );
   }
 }
 
-ReactDom.render(<AppContainer title={Title} />, document.getElementById("app"));
+ReactDom.render(<AppContainer title={title} history={history} />, document.getElementById("app"));
