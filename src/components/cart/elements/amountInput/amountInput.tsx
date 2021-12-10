d16b0fee2c8b0bf2f1f96aad6f7033340a2103aa
AsyaDeWitch <denisevich.asya@gmail.com>
2021-12-10 19:50:36 +0300

    Optimization. Rewrote 2 components to styled components: About page and amount input from cart table item

--- src/components/cart/elements/amountInput/amountInput.tsx
+++ src/components/cart/elements/amountInput/amountInput.tsx
@@ -1,5 +1,24 @@
 import { ChangeEventHandler, KeyboardEvent, memo } from "react";
-import "./amountInput.scss";
+import styles from "styled-components";
+
+const AmountInput = styles.input`
+  width: 8vw;
+  height: 1.4vw;
+  border-color: $basicFontColor;
+  border-radius: 5px;
+  border-width: 1px;
+  font-size: 1vw;
+  color: $basicFontColor;
+  background-color: transparent;
+  padding: 0 0 0 0.3vw;
+
+  &:focus {
+    border-width: 1px;
+    border-color: $basicFontColor;
+    border-radius: 5px;
+    outline: none;
+  }
+`;
 
 const MemoizedAmountInput = memo(
   (props: { onChange: ChangeEventHandler<HTMLInputElement>; value: number }): JSX.Element => {
@@ -8,7 +27,7 @@ const MemoizedAmountInput = memo(
     };
 
     return (
-      <input
+      <AmountInput
         className="amountInput"
         type="number"
         min="1"
