ffee6fc20b0a4adbb72718ae3a83c2cc1eda68e8
AsyaDeWich <asyadenisevich2001@gmail.com>
2021-12-23 17:29:01 +0300

    GameCardContainer.Used string template instead of concat

--- src/components/products/cards/gameCardsContainer.tsx
+++ src/components/products/cards/gameCardsContainer.tsx
@@ -8,7 +8,7 @@ const MemoizedGameCardsContainer = memo(
     <>
       <ul className="game__cards-container">
         {props.productItems.map((item: ProductItem) => (
-          <li key={item.id.toString().concat(item.name)} className="game__card-container" role="menuitem">
+          <li key={`${item.id.toString()}${item.name}}`} className="game__card-container" role="menuitem">
             <div className="game__card-holder">
               <GameCard key={item.id} productItem={item} image={item.logo} />
             </div>
